81ad4c29388deb3ca567b6768e60ee695ffda106
Thulasimani <110888405+thulasiman@users.noreply.github.com>
2024-05-02 16:10:51 +0530

    Add files via upload

---
+++ EDet.cs
@@ -0,0 +1,25 @@
+﻿using InsurencePolicy.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsurencePolicy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EDet : ControllerBase
+    {
+
+        private readonly InsurancePolicyContext _context;
+
+        public EDet(InsurancePolicyContext context) => _context = context;
+
+        [HttpGet]
+
+        public async Task<IEnumerable<EDetail>> GetUsers()
+        {
+            return await _context.EDetails.ToListAsync();
+        }
+
+    }
+}
---
+++ PayDetailsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InsurencePolicy.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsurencePolicy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PayDetailsController : Controller
+    {
+        private readonly InsurancePolicyContext _context;
+
+        public PayDetailsController(InsurancePolicyContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+
+        public async Task<IEnumerable<PayDetail>> GetUsers()
+        {
+            return await _context.PayDetails.ToListAsync();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> createPayDeatils([FromBody] PayDetail payDetail)
+        {
+            _context.Add(payDetail);
+            await _context.SaveChangesAsync();
+            return Ok(payDetail);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUserById(int id)
+        {
+            var dbUser = await _context.PayDetails.FindAsync(id);
+
+            if (dbUser == null)
+            {
+                return NotFound(); // Or appropriate action if user with given id is not found
+            }
+
+            _context.PayDetails.Remove(dbUser); // Corrected from _context..Remove(dbUser);
+
+            await _context.SaveChangesAsync();
+
+            return Ok("Successfully deleted!");
+        }
+    }
+}
